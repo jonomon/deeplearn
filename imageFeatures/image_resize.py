29133142fd792a87a1a908832ad0590cb87a3963
jonomon <jono.chung@gmail.com>
2016-03-15 19:36:08 -0400

    Added image features

---
+++ imageFeatures/image_resize.py
@@ -0,0 +1,21 @@
+"""
+Fast image resize script: DRD@Kaggle
+
+__author__ : Abhishek Thakur
+"""
+
+import os
+import glob
+from joblib import Parallel, delayed
+
+in_dir = 'data/'
+out_dir = 'data/resized_224/'
+IMAGE_SIZE = 224
+
+from PIL import Image, ImageChops
+JPEG_FILES = glob.glob(in_dir+'*.jpg')
+def convert(img_file):
+    im = Image.open(img_file)
+    im.resize((IMAGE_SIZE, IMAGE_SIZE)).save(out_dir + os.path.basename(img_file), 'JPEG')
+
+Parallel(n_jobs=32, verbose=10)(delayed(convert)(f) for f in JPEG_FILES)
---
+++ imageFeatures/keras_train.py
@@ -0,0 +1,105 @@
+## change directory variable to the directory of the images
+## output features will be in the outputFeatures folder named image_features.csv
+
+import pandas as pd
+import numpy as np
+from src import *
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import glob
+import os
+import h5py
+
+batch_size = 32
+nb_classes = 9
+nb_epoch = 200
+img_channels = 3
+img_rows = 224
+img_cols = 224
+
+def VGG_16(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,img_rows,img_cols)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(1000, activation='softmax'))
+
+    assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
+    f = h5py.File(weights_path)
+    for k in range(f.attrs['nb_layers']):
+        if k >= len(model.layers): # ignore the last layer
+            break
+        g = f['layer_{}'.format(k)]
+        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+        model.layers[k].set_weights(weights)
+    f.close()
+
+    return model
+
+def getOutput(model, filename):
+    from scipy.misc import imread, imresize, imsave
+    img = imresize(imread(filename), (img_rows, img_cols))
+    img = img.transpose((2, 0, 1)).astype('float64')
+    img[:, :, 0] -= 103.939
+    img[:, :, 1] -= 116.779
+    img[:, :, 2] -= 123.68
+    img = np.expand_dims(img, axis=0)
+    out = model.predict(img)
+    return out
+
+if __name__ == "__main__":
+    model = VGG_16('weights/vgg16_weights.h5')
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    
+    directory = "./data/" #input directory
+    features = []
+    filenames = []
+    input_images = glob.glob(directory+'/*jpg')
+    for i, filename in enumerate(input_images):
+        print("{0}/{1} Analysing {2}".format(i, len(input_images), filename))
+        output = getOutput(model, filename)
+        filenames.append(filename)
+        features.append(output)
+    f = pd.DataFrame(np.concatenate(features), index=filenames)
+    f.to_csv("outputFeatures/image_features.csv")
